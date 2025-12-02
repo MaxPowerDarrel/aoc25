4ed59baa774c0a9a6176e384796205be291240c5
Darrel Ross <MaxPowerDarrel@users.noreply.github.com>
2025-12-02 10:18:23 -0500

    Day2 Commit

---
+++ src/main/kotlin/Day2.kt
@@ -0,0 +1,30 @@
+data class Range(val start: Long, val end: Long) {
+    init {
+        require(start <= end) { "Start must be less than or equal to end" }
+    }
+
+    fun filter(f: (Long) -> Boolean): Sequence<Long> = sequence {
+        for (i in start..end) if (f(i)) yield(i)
+    }
+}
+
+fun isValidId(id: Long): Boolean {
+    val s = id.toString()
+    val length = s.length
+    return (1..length / 2).any { patternLength ->
+        length % patternLength == 0 && (patternLength until length step patternLength).all { offset ->
+            s.regionMatches(offset, s, 0, patternLength)
+        }
+    }
+}
+
+fun parseInput() = ClassLoader.getSystemResource("Day2.txt").readText().splitToSequence(",").flatMap { range ->
+        Range(
+            range.substringBefore("-").toLong(), range.substringAfter("-").toLong()
+        ).filter(::isValidId)
+    }
+
+
+fun main() {
+    println(parseInput().sum())
+}
