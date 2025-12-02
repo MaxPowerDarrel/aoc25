81485686a86b8a5a5de174cda4a7d5b84d5a47ea
Darrel Ross <MaxPowerDarrel@users.noreply.github.com>
2025-12-01 20:53:22 -0500

    Day1 Commit

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,27 +1,21 @@
-enum class Direction { LEFT, RIGHT }
+sealed class Direction(val delta: Int) {
+    object LEFT : Direction(-1)
+    object RIGHT : Direction(1)
+}
 
 data class Instruction(val direction: Direction, val steps: Int)
 
 class SecurityDevice(
-    private val maxPosition: Int = 99,
-    initialPosition: Int = 50
+    private val maxPosition: Int = 99, initialPosition: Int = 50
 ) {
     var passcode = 0
         private set
 
     private var currentPosition = initialPosition
 
-    fun turn(instruction: Instruction) {
-        val delta = when (instruction.direction) {
-            Direction.LEFT -> -1
-            Direction.RIGHT -> 1
-        }
-
-        repeat(instruction.steps) {
-            if (currentPosition == 0) passcode++
-
-            currentPosition = (currentPosition + delta).mod(maxPosition + 1)
-        }
+    fun turn(instruction: Instruction) = repeat(instruction.steps) {
+        if (currentPosition == 0) passcode++
+        currentPosition = (currentPosition + instruction.direction.delta).mod(maxPosition + 1)
     }
 }
 
@@ -34,12 +28,8 @@ private fun String.toInstruction(): Instruction = when (this.firstOrNull()) {
 fun main() {
     val device = SecurityDevice()
 
-    ClassLoader.getSystemResource("Day1.txt")
-        .readText()
-        .lineSequence()
-        .filter { it.isNotBlank() }
-        .map { it.toInstruction() }
-        .forEach { device.turn(it) }
+    ClassLoader.getSystemResource("Day1.txt").readText().lineSequence().filter { it.isNotBlank() }
+        .map { it.toInstruction() }.forEach { device.turn(it) }
 
     println("Passcode: ${device.passcode}")
 }
